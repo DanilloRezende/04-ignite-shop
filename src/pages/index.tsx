4cc94de64d36df15d77b8ed97e8e9c4c215d1eb0
DanilloRezende <danillo.rezende3@gmail.com>
2022-09-21 14:33:30 -0300

    Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,37 @@
-import { styled } from "../styles"
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  },
-})
+import camiseta1 from '../assets/camisetas/camisa1.png'
+import camiseta2 from '../assets/camisetas/camisa2.png'
+import camiseta3 from '../assets/camisetas/camisa3.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>teste</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt='Imagem de uma camisa' width={520} height={480}/>
+
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt='Imagem de uma camisa' width={520} height={480}/>
+
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta3} alt='Imagem de uma camisa' width={520} height={480}/>
+
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
