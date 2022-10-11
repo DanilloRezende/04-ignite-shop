e3a7f3e1fe5dc6ddd5b41a46f71c2721bfb07799
DanilloRezende <danillo.rezende3@gmail.com>
2022-10-11 12:29:22 -0300

    Produto & Checkout - Redirect no SSR

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -34,8 +34,18 @@ export default function Success({customerName, product}: SuccessProps ) {
 
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ query}) => {
-    const sessionId  = String(query.session_id);
+export const getServerSideProps: GetServerSideProps = async ({ query, params }) => {
+
+    if(!query.session_id) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+    
+    const sessionId  = String(query.session_id);    
 
     const session = await stripe.checkout.sessions.retrieve(sessionId, {
         expand: ['line_items', 'line_items.data.price.product']
